f4468fe3afa12276552c6fc8693b60b10e5641cb
longer <zinong.lzn@alibaba-inc.com>
2019-08-08 19:36:34 +0800

    springboot 化改造

--- smart-sso/smart-sso-server/src/main/java/com/smart/sso/server/config/FilterConfiguration.java
+++ smart-sso/smart-sso-server/src/main/java/com/smart/sso/server/config/FilterConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.DelegatingFilterProxy;
 
 /**
  * @FileName: FilterConfiguration
--- smart-sso/smart-sso-server/src/main/java/com/smart/sso/server/config/MvcConfiguration.java
+++ smart-sso/smart-sso-server/src/main/java/com/smart/sso/server/config/MvcConfiguration.java
@@ -4,7 +4,10 @@ import com.smart.sso.server.common.LocalTokenManager;
 import com.smart.sso.server.common.TokenManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @FileName: MvcConfiguration
